5c6135b3957543b6c4f6afc0195b46d7dac0c7e9
Daniel Smith <37248569+dndsmith@users.noreply.github.com>
2018-03-10 18:02:52 -0500

    Added Log struct

--- DServiceLocal.h
+++ DServiceLocal.h
@@ -2,6 +2,22 @@
 #define DSERVICELOCAL_H
 
 using namespace std;
+
+struct Log {
+	int glucoseLevel;
+	int carbIntake;
+	int insulinDosage;
+	int hour;
+	int minute;
+	Log(int g, int c, int i, int h, int m) {
+		glucoseLevel = g;
+		carbIntake = c;
+		insulinDosage = i;
+		hour = h;
+		minute = m;
+	}
+};
+
 class DServiceLocal{
 public:
   DServiceLocal();
