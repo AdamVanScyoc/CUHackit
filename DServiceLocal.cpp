fdea4cf364e29b66bfd2d489839ffae900fea83f
tyreek-w <tyreekw@g.clemson.edu>
2018-03-10 17:36:00 -0500

    File Setup Commit

---
+++ DServiceLocal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "DServiceLocal.h"
+
+using namespace std;
+DServiceLocal::DServiceLocal(){
+  cout << "Constructing...." << endl;
+}
+
+DServiceLocal::~DServiceLocal(){
+  cout << "Destructing...." << endl;
+}
---
+++ DServiceLocal.h
@@ -0,0 +1,10 @@
+#ifndef DSERVICELOCAL_H
+#define DSERVICELOCAL_H
+
+using namespace std;
+class DServiceLocal{
+public:
+  DServiceLocal();
+  ~DServiceLocal();
+};
+#endif
