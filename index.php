9e72e1b809e0d1b904bb72728368a1eca6107c45
hgerrald <32804403+hgerrald@users.noreply.github.com>
2018-03-11 00:03:08 -0500

    Add files via upload
    tried to fix error

--- index.php
+++ index.php
@@ -13,9 +13,13 @@
     echo "<br>";
     echo "Insulin is: " . $_GET["Insulin"];
     echo "<br>";
-    echo "Time since last meal is: " . $_GET["Hours"] . " " .  "Hours" . " " .
-                    $_GET["Minutes"] . "Minutes";
+    echo "Time since last meal is: " . $_GET["Hours"] . " " .  "Hours," . " " .
+                    $_GET["Minutes"] . " " . "Minutes";
 
+    echo "<br>";
+    echo "<br>";
+    echo "Thank you. Your data has been recorded.";
+    echo "<br>";    
 
     $my_file = fopen("dataFile.txt", "a") or die("Unable to open file!");
     $new_line = "\n";
@@ -32,7 +36,7 @@
     fwrite($my_file, $_GET["Minutes"]);
     fwrite($my_file, $new_line); 
 
-    flcose($my_file); 
+    fclose($my_file); 
    ?>
 
 
