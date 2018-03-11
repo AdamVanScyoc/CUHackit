40bd9c660ea591a2e3251f9ace8aa7bb40aed139
hgerrald <32804403+hgerrald@users.noreply.github.com>
2018-03-10 23:02:38 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<title>Diabetic Spreadsheet</title>
+  </head>
+  
+  <?php
+    echo "Day is: " . $_GET["Day"];
+    echo "<br>";
+    echo "Glucose is: " . $_GET["Glucose"];
+    echo "<br>";
+    echo "Carb intake is: " . $_GET["CarbIntake"];
+    echo "<br>";
+    echo "Insulin is: " . $_GET["Insulin"];
+    echo "<br>";
+    echo "Time since last meal is: " . $_GET["Hours"] . " " .  "Hours" . " " .
+                    $_GET["Minutes"] . "Minutes";
+
+
+    $my_file = fopen("dataFile.txt", "a") or die("Unable to open file!");
+    $new_line = "\n";
+    fwrite($my_file, $_GET["Day"]);
+    fwrite($my_file, $new_line);
+    fwrite($my_file, $_GET["Glucose"]);
+    fwrite($my_file, $new_line);
+    fwrite($my_file, $_GET["CarbIntake"]);
+    fwrite($my_file, $new_line);
+    fwrite($my_file, $_GET["Insulin"]);
+    fwrite($my_file, $new_line);
+    fwrite($my_file, $_GET["Hours"]);
+    fwrite($my_file, $new_line);
+    fwrite($my_file, $_GET["Minutes"]);
+    fwrite($my_file, $new_line); 
+
+    flcose($my_file); 
+   ?>
+
+
+</body>
+</html>
+
