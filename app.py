bc523ba43c6179f2632ad5f421b98fb88f18234c
Theriley106 <christopherlambert106@gmail.com>
2018-03-11 03:51:56 -0400

    Added the databse

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('UTF8')
 from flask import Flask, request, jsonify, render_template, request, url_for, redirect, Markup, Response, send_file, send_from_directory, make_response
-
+import json
 
 app = Flask(__name__,template_folder="templates/",static_url_path='/static')
 
@@ -12,7 +12,9 @@ def main():
 
 @app.route('/submitForm', methods=["POST"])
 def addData():
-	print(request.form.to_dict())
+	sample = request.form.to_dict()
+	with open('database.json', 'a') as fp:
+		json.dump(sample + "\n", fp)
 	return render_template('index.html')
 
 if __name__ == "__main__":
