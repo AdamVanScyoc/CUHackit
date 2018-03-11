dfc59a6071a95ae2159d588ba6c36b73e8fcd110
Theriley106 <christopherlambert106@gmail.com>
2018-03-11 03:22:30 -0400

    Added form method

--- app.py
+++ app.py
@@ -12,7 +12,5 @@ app = Flask(__name__,template_folder="templates/",static_url_path='/static')
 def main():
 	return render_template('index.html')
 
-
-
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5000)
